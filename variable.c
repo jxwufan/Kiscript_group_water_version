18a938eafb766061eea5a8367868d8b82073d047
fan wu <jxwufan@gmail.com>
2016-01-06 23:15:57 +0800

    implement new variable

--- variable.c
+++ variable.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 #include "variable.h"
 
 void variable_out_of_scope(variable_t *variable) {
@@ -13,12 +14,11 @@ void variable_out_of_scope(variable_t *variable) {
     }
 }
 
-variable_t *variable_new(variable_type_t variable_type, gpointer *variable_data, GHashTable *attribute_hash_table,
+variable_t *variable_new(variable_type_t variable_type, gpointer variable_data,
                               activation_record_t *AR) {
     variable_t *variable = (variable_t*) GC_malloc(sizeof(variable_t));
     variable->variable_type = variable_type;
     variable->variable_data = variable_data;
-    variable->attribute_hash_table = attribute_hash_table;
     variable->AR = AR;
     return variable;
 }
@@ -31,7 +31,7 @@ variable_t *variable_clone(variable_t *variable) {
         new_variable->AR = variable->AR;
         g_hash_table_ref(variable->AR->AR_hash_table);
     }
-    return NULL;
+    return new_variable;
 }
 
 void variable_on_destory(variable_t *variable) {
@@ -58,12 +58,44 @@ gchar* variable_to_string(variable_t *variable) {
 
 void variable_free(variable_t *variable) {
     if (variable->variable_data != NULL) {
-        GC_FREE(variable->variable_data);
+        if (variable->variable_type != VARIABLE_OBJECT) {
+            GC_FREE(variable->variable_data);
+        } else {
+            g_hash_table_unref((GHashTable*) variable->variable_data);
+        }
     }
     if (variable->AR != NULL) {
         g_hash_table_unref(variable->AR->AR_hash_table);
     }
-    if (variable->attribute_hash_table != NULL) {
-        g_hash_table_unref(variable->attribute_hash_table);
-    }
+}
+
+variable_t *variable_null_new() {
+    return variable_new(VARIABLE_NULL, NULL, NULL);
+}
+
+variable_t *variable_bool_new(gpointer bool_data) {
+    return variable_new(VARIABLE_BOOL, GC_malloc(sizeof(gboolean)), NULL);
+}
+
+variable_t *variable_numerical_new(gpointer numerical_data) {
+    gdouble *tmp_double = (gdouble*) GC_malloc(sizeof(gdouble));
+    *tmp_double = *((gdouble*) numerical_data);
+    return variable_new(VARIABLE_NUMERICAL, tmp_double, NULL);
+}
+
+variable_t *variable_string_new(gpointer string_data) {
+    guint str_len = strlen((gchar*) string_data);
+    gchar *str = GC_malloc(str_len * sizeof(gchar));
+    memcpy(str, string_data, str_len);
+    return variable_new(VARIABLE_STRING, str, NULL);
+}
+
+variable_t *variable_object_new() {
+    GHashTable *attribute_table = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, (GDestroyNotify) variable_on_destory);
+
+    return variable_new(VARIABLE_OBJECT, attribute_table, NULL);
+}
+
+variable_t *variable_function_new(gpointer function_data, activation_record_t *AR) {
+    return variable_new(VARIABLE_FUNC, function_data, AR);
 }
--- variable.h
+++ variable.h
@@ -24,18 +24,17 @@ typedef enum {
 typedef struct {
     variable_type_t variable_type;
     gpointer        variable_data;
-    GHashTable      *attribute_hash_table;
     activation_record_t *AR;
 } variable_t;
 
-variable_t  *variable_new           (variable_type_t variable_type, gpointer *variable_data, GHashTable *attribute_hash_table, activation_record_t *AR);
+variable_t  *variable_new           (variable_type_t variable_type, gpointer variable_data, activation_record_t *AR);
 variable_t  *variable_null_new      ();
-variable_t  *variable_bool_new      (gpointer *bool_data);
-variable_t  *variable_numerical_new (gpointer *numerical_data);
-variable_t  *variable_string_new    (gpointer *string_data);
-variable_t  *variable_object_new    (gpointer *object_data);
-variable_t  *variable_exception_new (gpointer *exception_data);
-variable_t  *variable_function_new  (gpointer *function_data, activation_record_t *AR);
+variable_t  *variable_bool_new      (gpointer bool_data);
+variable_t  *variable_numerical_new (gpointer numerical_data);
+variable_t  *variable_string_new    (gpointer string_data);
+variable_t  *variable_object_new    ();
+variable_t  *variable_exception_new (gpointer exception_data); // TODO: exception
+variable_t  *variable_function_new  (gpointer function_data, activation_record_t *AR);
 variable_t  *variable_clone         (variable_t *variable);
 
 void        variable_on_destory     (variable_t *variable);
